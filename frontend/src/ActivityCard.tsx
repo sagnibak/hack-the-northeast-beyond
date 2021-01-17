41bbe388d46484c536e51183111ce52cb27d02c2
Sagnik Bhattacharya <sagnikdps@gmail.com>
2021-01-17 10:53:08 -0800

    doneeee

--- frontend/src/ActivityCard.tsx
+++ frontend/src/ActivityCard.tsx
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "./ActivityCard.css"
+import axios from "axios"
 
 type Direction = "left" | "right" | "up" | "down"
 
@@ -9,17 +10,55 @@ interface Props {
 }
 
 const ActivityCard: React.FC<Props> = ({ type, onClick }) => {
+  const [jokeState, setJokeState] = useState("")
+  const [titleState, setTitleState] = useState("")
+
+  useEffect(() => {
+    axios
+      .get("https://icanhazdadjoke.com/", {
+        headers: {
+          Accept: "application/json",
+        },
+      })
+      .then((res) => setJokeState(res.data.joke))
+
+    axios
+      .get("https://icanhazdadjoke.com/", {
+        headers: {
+          Accept: "application/json",
+        },
+      })
+      .then((res) => {
+        const joke = res.data.joke.split(" ")
+        console.log(joke)
+        const random = Math.floor(Math.random() * joke.length)
+        const random2 = Math.floor(Math.random() * joke.length)
+        setTitleState(joke[random] + " " + joke[random2])
+      })
+    // axios
+    //   .get("https://evilinsult.com/generate_insult.php", {
+    //     headers: {
+    //       Accept: "application/json",
+    //     },
+    //   })
+    //   .then((res) => {
+    //     setTitleState(res.data.insult)
+    //   })
+
+    return () => {}
+  }, [])
+
   return (
     <div className={`flexcard ${type}`}>
       {" "}
       {/* <div style={{ filter: "brightness(80%)" }}> */}
-      <h2>Foo</h2>
+      <h2>{titleState}</h2>
       <em>
-        <small>Some small text</small>
+        <small>A dad joke for you, because we aren't so evil</small>
       </em>
       {/* </div> */}
       <hr />
-      <p>A bit more text, this text should be long long long.</p>
+      <p>{jokeState}</p>
       <button onClick={onClick} className="big-button">
         Click me if you can!
       </button>
